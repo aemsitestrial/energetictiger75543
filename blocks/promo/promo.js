ba8a451fbc4803f1086fb7e267f90ee4defeea2a
Bala R <2962753@tcs.com>
2026-09-17 15:21:03 +0530

    blocks added

--- blocks/promo/promo.js
+++
@@ -1,8 +0,0 @@
-export default function decorate(block) {
-  // Validate the block element exists
-  if (!block || !(block instanceof Element)) {
-    console.warn('Invalid block element passed to decorate()');
-    return;
-  }
-  block.classList.add('promo-loaded');
-}
